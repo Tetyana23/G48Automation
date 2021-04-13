ff3b7ea5d08cb70310c8a4480193d763b2f1d6e0
tatianakurylenko <Tetyana.Kurylenko@defacto.com>
2021-04-13 21:27:44 +0100

    new changes

---
+++ src/main/java/helpers/fileHelper.java
@@ -0,0 +1,60 @@
+package helpers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.write;
+
+public class fileHelper {
+
+    private static final Logger log = LogManager.getLogger("Обработчик файлов");
+
+    public static List<String> readFile(String path) {
+        try {
+            log.debug("Читаю файл " + path);
+            return readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            log.error(e);
+            return new ArrayList<>();
+        }
+    }
+
+    public static List<String> readFile(File file) {
+        try {
+            log.debug("Читаю файл " + file.getPath());
+            return readAllLines(file.toPath());
+        } catch (IOException e) {
+            log.error(e);
+            return new ArrayList<>();
+        }
+    }
+
+
+
+    public static File writeFileAndGet(List<String> data, String name) {
+        log.debug("Пишем файл " + name);
+        File parent = new File("/Users/tatianakurylenko/IdeaProjects/G48AutomationTanya2/target/outFiles");
+        if (!parent.exists()) {
+            log.debug("Создаю недостающие директории");
+            parent.mkdirs();
+        }
+        File result = new File(parent, name);
+        try {
+            log.debug("Произвожу запись в файл: " + result.getPath());
+            write(result.toPath(), data);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return result;
+    }
+
+
+}
